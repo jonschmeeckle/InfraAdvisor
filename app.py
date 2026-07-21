0afab04db70b6768d1956a5a2fd8a5a4114c8bdb
Jon Schmeeckle <jschmeeckle@gmail.com>
2026-07-21 16:57:03 -0400

    Add interactive menu system

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from banner import display_banner
+from menu import display_menu
+from menu import get_menu_choice
 def main():
     version = "0.1"
 
     display_banner(version)
 
+    while True:
+        display_menu()
+
+        choice = get_menu_choice()
+
+        print()
+
+        if choice == "4":
+            print("Exiting InfraAdvisor.")
+            break
+        else:
+            print(f"You selected option {choice}.")
+            print()
 
 if __name__ == "__main__":
     main()
