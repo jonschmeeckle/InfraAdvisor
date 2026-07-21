3577e1ca5390d8a0e8910d2be2ab0e2687e1514d
Jon Schmeeckle <jschmeeckle@gmail.com>
2026-07-21 16:53:59 -0400

    Add interactive menu system

---
+++ menu.py
@@ -0,0 +1,10 @@
+def display_menu():
+    print()
+    print("1. Analyze Cisco Configuration")
+    print("2. Analyze Palo Alto Configuration")
+    print("3. Generate Executive Summary")
+    print("4. Exit")
+    print()
+
+def get_menu_choice():
+    return input("Select an option: ")
